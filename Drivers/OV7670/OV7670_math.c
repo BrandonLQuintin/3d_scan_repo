205d9f2c93ef416e96ac2638c517a2f18683ef66
Brandon Quintin <brandonleequintin@gmail.com>
2026-05-09 20:09:42 +0900

    feat: basic pixel filtering

--- Drivers/OV7670/OV7670_math.c
+++ Drivers/OV7670/OV7670_math.c
@@ -3,6 +3,11 @@
 
 uint16_t brightest_pixels[OV7670_QVGA_HEIGHT] = {0};
 
+typedef struct {
+    uint8_t leastBrightestRed;
+    uint16_t bestX[5];
+} RedPixelScans;
+
 static uint16_t getPixel(uint32_t* frameBuffer, int x, int y) {
     int pixel_index = y * RESOLUTION_X + x;
     // uint32_t packs 2 RGB565 pixels in one, so we must determine where our uint16_t pixel is.
@@ -15,18 +20,29 @@ static uint8_t getRed(uint16_t pixel) {
     return (pixel >> 11) & 0x1F;
 }
 
+static void compareRedPixels(RedPixelScans *input, uint8_t current_red_pixel, uint8_t current_x){
+    if (current_red_pixel > input->leastBrightestRed){
+        input->leastBrightestRed = current_red_pixel;
+        for (int i = sizeof(input->bestX) / sizeof(input->bestX[0]); i >= 1; i--) {
+            input->bestX[i] = input->bestX[i - 1];
+        }
+        input->bestX[0] = current_x;
+    }
+}
+
 void ov7670_findBrightestPixels(uint32_t* frameBuffer, bool isSecondHalf){
     uint8_t offset = (isSecondHalf) ? RESOLUTION_Y / 2 : 0;
     for (int y = 0; y < RESOLUTION_Y / 2; y++){
-        uint8_t maxRed = 0;
+        RedPixelScans topRedPixels = {0};
         uint16_t bestX = 0;
         for (int x = 0; x < RESOLUTION_X; x++){
             uint8_t red = getRed(getPixel(frameBuffer, x, y));
-            if (red > maxRed) {
-                maxRed = red;
-                bestX = x;
-            }
+            compareRedPixels(&topRedPixels, red, x);
         }
+
+        // Choosing the middle of the top brightest red pixels will result in less noisy data being passed through.
+        uint8_t arraySize = sizeof(topRedPixels.bestX) / sizeof(topRedPixels.bestX[0]);
+        bestX = topRedPixels.bestX[arraySize / 2];
         brightest_pixels[offset + y] = bestX;
     }
 }
